2a31fdd07b0e51a098faa4559f3585e7b1cd0e6e
Aidan Jude <aidanjude2016@gmail.com>
2018-11-26 03:03:07 -0600

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,11 +1,22 @@
+#Author: Aidan Jude
+#Date: 11/26/2018
+#Description:
+    #This portion of the TCP client/server program is responsible for sending TCP messages
+    #and receiving the response from the server. Since the basic function that the server computes is to
+    #compute postfix operations, we send messages like so:
+        #Example message that is sent => 1428+*+
+    #This is equal to 1+(4*(2+8)), so the expected response from the server would be 41
+
 import socket
 import time
 
-HOST = "192.168.1.107"
-PORT = 9000
+HOST = "SERVER_IP_ADDRESS"
+PORT = 9000 #choose a port number
 
+#utilize the socket library to create a socket object
 server_info = (HOST, PORT)
 sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+#establish a connection with the server
 sock.connect(server_info)
 print("TCP Connection Established\n")
 msg = ''
@@ -17,6 +28,7 @@ while True:
         print("Please enter the line you want to pass to the server")
         print("If this is the last line, enter -1")
         raw_input = input()
+        #if the user has no more lines of postfix operation to compute, we terminate the loop
         if raw_input == '-1':
             break
         if msg == '':
@@ -24,9 +36,11 @@ while True:
         else:
             msg = msg + '\n' + raw_input
     print('sending:\n' + msg)
+    #we encode the message to follow the TCP protocol byte format
     byte_msg = msg.encode()
     sock.send(byte_msg)
     msg = ''
+    #we read the buffer for the server's response
     data = sock.recv(1024)
     print('received:\n' + data.decode())
     print("Would you like to kill this session? (y or n)")
--- Server.py
+++ Server.py
@@ -1,6 +1,13 @@
+#Author: Aidan Jude
+#Date: 11/26/2018
+#Description:
+    #This portion of the TCP client/server program is responsible for receiving TCP messages
+    #and sending the appropriate response. 
+
 import socket
 import time
 
+#this function handles the postfix computation by utilizing a list like that of a stack
 def eval_postfix(eval_line):
     s = list()
     for symbol in eval_line:
@@ -26,16 +33,20 @@ def eval_postfix(eval_line):
     return s.pop()
 
 
-HOST = "192.168.1.107"
+HOST = "SERVER_IP_ADDRESS"
 PORT = 9000
 NUMBER_OF_SOCKETS = 1
 BUFFER_SIZE = 1024
 server_info = (HOST, PORT)
 sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+#we set the socket option to reuseable address of 1 so that we can quickly assign the socket after
+#the previous socket (session of sorts) is terminated.
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 print("Server starting...")
+#bind the socket we have assigned and listen for clients
 sock.bind(server_info)
 sock.listen(NUMBER_OF_SOCKETS)
+#accept the client connection
 connection, client_address = sock.accept()
 cnt = 0
 return_lines = list()
@@ -44,9 +55,15 @@ data = ''
 while True:
     print("Connection Address: " + str(client_address))
     while True:
+        #read the data from the buffer between the client and server
         data = connection.recv(BUFFER_SIZE)
+        #decode the data into a usable string format
         data = data.decode()
+        #if the buffer is empty, we kill the loop and go read again
         if not data: break;
+        #if the client notifies the server of an impending termination, we
+        #give the client a couple of seconds to close its connection (gentler if client
+        #goes first) and then we close the socket and connection
         if data == 'KILL':
             time.sleep(2.0)
             connection.close()
@@ -56,6 +73,10 @@ while True:
             break
         if data != None:
             print('message received:\n' + data)
+            #break the lines up and compute the postfix operations
+            #we then assign the values to a string for a return message and a list
+            #structure (return_lines) if this were to be needed to properly address
+            #individual results
             sent_lines = data.splitlines()
             for cnt in range(len(sent_lines)):
                 return_lines.append(eval_postfix(sent_lines[cnt]))
@@ -64,13 +85,13 @@ while True:
                 ++cnt
             cnt = 0
             print(return_message)
+            #send the results back to the client
             connection.send(return_message.encode())
             return_lines.clear()
             return_message = ""
+    #the client has informed the server of its closing its connection, so we clear the data
+    #variable and re-establish the socket and wait for any new connections to be made with appropriate clients
     data = ''
-    #print("Must wait at least 90 seconds before reusing same port and socket")
-    #time.sleep(100)
-    #print("Wait complete. Can now use socket and port")
     sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(server_info)
